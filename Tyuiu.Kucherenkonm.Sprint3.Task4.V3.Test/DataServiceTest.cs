47334868dbb1820ee86095cd8deb9d463e619340
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-10-26 22:20:01 +0500

    Task4

---
+++ Tyuiu.Kucherenkonm.Sprint3.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.Kucherenkonm.Sprint3.Task4.V3.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint3.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 1.067;
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint3.Task4.V3/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.Kucherenkonm.Sprint3.Task4.V3.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема:     Использование операторов continue и break в циклах            *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*   На отрезке, где x принимает значения от -5 до 5, вычислить значение   *");
+        Console.WriteLine("*    функции. При х = 0 прервать цикл. Полученные значения суммировать    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine(" Старт ряда " + startValue);
+        Console.WriteLine(" Конец ряда " + stopValue);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Сумма ряда = " + ds.Calculate(startValue,stopValue));
+
+        Console.ReadKey();
+
+    }
+}
