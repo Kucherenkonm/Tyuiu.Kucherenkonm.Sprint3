8f89d0721b68fb0aa4edddc0d9954633c1f39c7c
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-10-26 21:48:11 +0500

    Task3

---
+++ Tyuiu.Kucherenkonm.Sprint3.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.Kucherenkonm.Sprint3.Task3.V21.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint3.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidReplaceNumOnChar()
+        {
+            DataService ds = new DataService();
+            string str = "f3g5ht g4j 34kg4";
+            char chr = 'e';
+            string res = ds.ReplaceNumOnChar(str,chr);
+            string wait = "fegeht gej eekge";
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint3.Task3.V21/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.Kucherenkonm.Sprint3.Task3.V21.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема:                   Оператор цикла foreach                          *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*  Написать программу на C#, используя цикл foreach, которая заменяет     *");
+        Console.WriteLine("*                     цифры в строке на букву е                           *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string str = "f3g5ht g4j 34kg4";
+        char chr = 'e';
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.ReplaceNumOnChar(str,chr));
+        Console.ReadKey();
+
+    }
+}
