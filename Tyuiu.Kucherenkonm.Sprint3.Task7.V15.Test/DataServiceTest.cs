cda3b7ddaeb1c689949a84546da407d7ba787451
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-10-27 01:07:09 +0500

    Task7

---
+++ Tyuiu.Kucherenkonm.Sprint3.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.Kucherenkonm.Sprint3.Task7.V15.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint3.Task7.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = 3.63;
+            valueWaitArray[1] = -11.70;
+            valueWaitArray[2] = -14.02;
+            valueWaitArray[3] = -5.24;
+            valueWaitArray[4] = 1.32;
+            valueWaitArray[5] = 0.00;
+            valueWaitArray[6] = -1.32;
+            valueWaitArray[7] = 5.24;
+            valueWaitArray[8] = 14.02;
+            valueWaitArray[9] = 11.70;
+            valueWaitArray[10] = -3.63;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(res, valueWaitArray);
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint3.Task7.V15/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.Kucherenkonm.Sprint3.Task7.V15.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #3 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема:       Добавление к решению итоговых проектов по спринту           *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*       Напишите программу, которая вывдит таблицу значений функции       *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine(" Старт ряда " + startValue);
+        Console.WriteLine(" Конец ряда " + stopValue);
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+        double[] valueArray;
+        valueArray = new double[len];
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("+--------------+--------------+");
+        Console.WriteLine("|--------------|--------------|");
+        Console.WriteLine("+--------------+--------------+");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine("|{0,6:d}        |     {1,6:f2}   |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+--------------+--------------+");
+
+        Console.ReadKey();
+
+    }
+}
